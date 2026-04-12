1993a6e1039b75cc59d6123e2b135bf3cc281df0
painnick <painnick@gmail.com>
2026-04-12 23:47:52 +0900

    feat: define hardware pin configuration and update project requirements

--- main/pin_config.h
+++ main/pin_config.h
@@ -8,21 +8,22 @@
 #define TRACK_BIN2_PIN  33
 
 // Motor Driver #2: Turret and Po-Mount
-// Physical sequence: 22, 21, 19, 18
+// Tracks & Turret & Port Mount (DRV8833)
 #define TURRET_AIN1_PIN 22
 #define TURRET_AIN2_PIN 21
-#define MOUNT_BIN1_PIN  19
-#define MOUNT_BIN2_PIN  18
 
-// Servo
-#define CANNON_SERVO_PIN 32
+#define PORT_BIN1_PIN   19
+#define PORT_BIN2_PIN   18
 
 // LEDs
 #define MG_LED_PIN        16
-#define HEADLIGHT_LED_PIN 17
-#define CANNON_LED_PIN    5
+#define HEADLIGHT_LED_PIN 5
+#define CANNON_LED_PIN    17
 #define BACKLIGHT_LED_PIN 4
 
 // Sound (JQ6500)
 #define SOUND_TX_PIN 23
 #define SOUND_UART_PORT UART_NUM_1
+
+// Servo
+#define CANNON_SERVO_PIN 32
